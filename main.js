819b0a32ff28bf8ba84c34dd4210e50ef833650c
samridhiravikumar <80625226+samridhiravikumar@users.noreply.github.com>
2022-04-22 17:08:52 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload(){
+}
+
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.position(500,200);
+}
+
+function draw(){
+}
+
+function take_snapshot(){
+    save('MyFilterImage.png');
+}
